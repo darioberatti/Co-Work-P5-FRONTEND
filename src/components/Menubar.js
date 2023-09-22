5f109524c881de489d4a6701f0b5a11bcad80b8a
Mateo <mateo.navarro98@gmail.com>
2023-09-22 09:59:50 -0300

    Menubar component

---
+++ src/components/Menubar.js
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import * as Menubar from "@radix-ui/react-menubar";
+import Link from "next/link";
+
+export default function Menu() {
+  return (
+    <Menubar.Root className="MenubarRoot">
+      <Link href={"/"}>
+        <Menubar.Menu>
+          <Menubar.Trigger className="MenubarTrigger">Home</Menubar.Trigger>
+        </Menubar.Menu>
+      </Link>
+      <Link href={"/register"}>
+        <Menubar.Menu>
+          <Menubar.Trigger className="MenubarTrigger">Register</Menubar.Trigger>
+        </Menubar.Menu>
+      </Link>
+
+      <Link href={"/users"}>
+        <Menubar.Menu>
+          <Menubar.Trigger className="MenubarTrigger">Users</Menubar.Trigger>
+        </Menubar.Menu>
+      </Link>
+    </Menubar.Root>
+  );
+}

373717fca99c0855cc5bf4947b26e3a7bdb03a38
Dario Beratti <darioberatti@gmail.com>
2023-09-28 12:50:53 -0300

    Footer

---
+++ src/components/MyFooter.js
@@ -0,0 +1,16 @@
+export default function MyFooter() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "50px",
+        color: "white",
+        backgroundColor: "rgb(0, 31, 85)",
+      }}
+    >
+      © 2023 Cowork P5
+    </div>
+  );
+}

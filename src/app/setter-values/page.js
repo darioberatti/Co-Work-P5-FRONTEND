ea00a43f2d4c27947a94f4a6e6120161c3cfa31f
Agustin Torroja <atorroja18@gmail.com>
2023-10-03 12:46:09 -0300

    restricted access to routes, depending on user role

--- src/app/setter-values/page.js
+++ src/app/setter-values/page.js
@@ -1,6 +1,18 @@
+"use client"
+
 import { SetterValues } from "@/components/SetterValues";
+import { useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
 
 export default function SetterValuesView() {
+
+  const router = useRouter()
+  const user = useSelector((state) => state.user.value);
+  
+  if( user.role !== "admin"){
+    router.push("/home")
+  }
+
   return (
     <div>
       <SetterValues />

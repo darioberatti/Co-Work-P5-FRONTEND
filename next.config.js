68d191aa0d03289699bbba42946183258692724d
Dario Beratti <darioberatti@gmail.com>
2023-10-10 18:26:23 -0300

    Changes canceled

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withImages = require("next-images");
-const withCSS = require("@zeit/next-css");
+const nextConfig = {
+  env: {
+    API_URL: process.env.API_URL,
+  },
+};
 
-module.exports = withImages(
-  withCSS({
-    serverRuntimeConfig: {
-      // Configuración de cookies del servidor
-      cookieSecret: "milanesa",
-      cookieName: "token", // Nombre de la cookie
-      cookieOptions: {
-        httpOnly: true, // Solo accesible en el servidor
-        sameSite: "strict", // Controla las restricciones de SameSite
-        secure: true, // Configura secure en producción
-      },
-    },
-    env: {
-      API_URL: process.env.API_URL,
-    },
-  })
-);
+module.exports = nextConfig;

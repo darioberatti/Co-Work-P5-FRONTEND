ec512c94512fadbce342a997df8612a4fc9a59d5
Dario Beratti <darioberatti@gmail.com>
2023-10-10 18:24:24 -0300

    testing changes in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    API_URL: process.env.API_URL,
-  },
-  publicRuntimeConfig: {
-    cookie: {
-      name: "token", // Nos aseguramos que la cookie tome el mismo nombre en local y en deployed
-    },
-  },
-};
+const withImages = require("next-images");
+const withCSS = require("@zeit/next-css");
 
-module.exports = nextConfig;
+module.exports = withImages(
+  withCSS({
+    serverRuntimeConfig: {
+      // Configuración de cookies del servidor
+      cookieSecret: "milanesa",
+      cookieName: "token", // Nombre de la cookie
+      cookieOptions: {
+        httpOnly: true, // Solo accesible en el servidor
+        sameSite: "strict", // Controla las restricciones de SameSite
+        secure: true, // Configura secure en producción
+      },
+    },
+    env: {
+      API_URL: process.env.API_URL,
+    },
+  })
+);

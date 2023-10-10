ef9c55eb0f9de81ccaa7a38e2af2d955e3e9fdbb
Dario Beratti <darioberatti@gmail.com>
2023-10-10 18:15:22 -0300

    Testing changes for deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
   },
+  publicRuntimeConfig: {
+    cookie: {
+      name: "token", // Nos aseguramos que la cookie tome el mismo nombre en local y en deployed
+    },
+  },
 };
 
 module.exports = nextConfig;
